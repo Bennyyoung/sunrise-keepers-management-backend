1b02ea510057a62b00aeec6ac1656ea119d24bbc
Bennyyoung <benjamineffiong124@yahoo.com>
2021-12-09 15:26:46 +0100

    Connected to the mongodb cloud

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongo_uri');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
 	try {
